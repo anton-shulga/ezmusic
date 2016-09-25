fc44a62e7efba75dad5a0ac986ff2fbe5d7d3928
znexie <znexxxx@gmail.com>
2016-09-25 19:41:12 +0300

    add log4j2 logging

--- src/by/epam/webpoject/ezmusic/constant/FilePath.java
+++ src/by/epam/webpoject/ezmusic/constant/FilePath.java
@@ -7,6 +7,6 @@ public class FilePath {
     public static final String DEFAULT_USER_PHOTO = "img/user/default_photo.jpg";
     public static final String DEFAULT_ALBUM_PHOTO = "img/album/default_photo.jpg";
     public static final String DEFAULT_AUTHOR_PHOTO = "img/author/default_photo.jpg";
-    public static final String DB_CONFIGURATION = "property.db";
+    public static final String DB_CONFIGURATION = "property.dfdfb";
     public static final String DEFAULT_SONG_FILE = "audio/song/default_song_file.mp3";
 }
